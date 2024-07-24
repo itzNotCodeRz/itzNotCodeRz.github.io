a722bd3a5c467ca5a561fff49a4482e18f8d47ef
itzNotCodeRz <121714623+itzNotCodeRz@users.noreply.github.com>
2024-07-24 18:06:18 +0530

    Create project.dart

---
+++ models/project.dart
@@ -0,0 +1,14 @@
+class Project {
+  final String title;
+  final String description;
+  final String imageUrl;
+  final String projectUrl;
+
+  Project({
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.projectUrl,
+  });
+}
+
